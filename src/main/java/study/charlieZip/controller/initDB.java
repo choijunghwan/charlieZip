fe9dc1d7f771bf349630810940f80eb3600a0abf
ChoiJungHwan <vkdlxj3562@naver.com>
2021-09-17 16:09:11 +0900

    ãH2 DB Connect

--- src/main/java/study/charlieZip/controller/initDB.java
+++ src/main/java/study/charlieZip/controller/initDB.java
@@ -32,72 +32,72 @@ public class initDB {
 
         @Transactional
         public void init() {
-//            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//
-//            Member member1 = Member.builder()
-//                    .username("vkdlxj3562")
-//                    .password(passwordEncoder.encode("1234"))
-//                    .date("19960105")
-//                    .gender(Gender.MAN)
-//                    .build();
-//
-//            Coffee_Board coffee_board1 = Coffee_Board.builder()
-//                    .store_name("벙커컴퍼니")
-//                    .menu_name("하프앤하프")
-//                    .price(7000)
-//                    .sweet(50)
-//                    .acidity(10)
-//                    .body(50)
-//                    .balance(50)
-//                    .aftertaste(50)
-//                    .aroma(10)
-//                    .desc("식물성 크림과 동물성크림을 섞어 만든 달달한 아인슈페너 느낌")
-//                    .build();
-//
-//            Coffee_Board coffee_board2 = Coffee_Board.builder()
-//                    .store_name("벙커컴퍼니")
-//                    .menu_name("쥬시롱블랙")
-//                    .price(5500)
-//                    .sweet(35)
-//                    .acidity(30)
-//                    .body(50)
-//                    .balance(50)
-//                    .aftertaste(50)
-//                    .aroma(45)
-//                    .desc("묵직한 바디감이 느껴지고 다크하지만 중간에 적절한 베리한 신맛이 느껴진다.")
-//                    .build();
-//
-//            Coffee_Board coffee_board3 = Coffee_Board.builder()
-//                    .store_name("커피몽타주")
-//                    .menu_name("아메리카노")
-//                    .price(4000)
-//                    .sweet(30)
-//                    .acidity(20)
-//                    .body(40)
-//                    .balance(50)
-//                    .aftertaste(40)
-//                    .aroma(40)
-//                    .desc("아직 안마셔봄 ㅋㅋ")
-//                    .build();
-//
-//            Coffee_Board coffee_board4 = Coffee_Board.builder()
-//                    .store_name("이디야")
-//                    .menu_name("아메리카노")
-//                    .price(3300)
-//                    .sweet(30)
-//                    .acidity(10)
-//                    .body(30)
-//                    .balance(30)
-//                    .aftertaste(20)
-//                    .aroma(20)
-//                    .desc("에스프레소 농도가 연하다.")
-//                    .build();
-//
-//            em.persist(member1);
-//            em.persist(coffee_board1);
-//            em.persist(coffee_board2);
-//            em.persist(coffee_board3);
-//            em.persist(coffee_board4);
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+            Member member1 = Member.builder()
+                    .username("vkdlxj3562")
+                    .password(passwordEncoder.encode("1234"))
+                    .date("19960105")
+                    .gender(Gender.MAN)
+                    .build();
+
+            Coffee_Board coffee_board1 = Coffee_Board.builder()
+                    .store_name("벙커컴퍼니")
+                    .menu_name("하프앤하프")
+                    .price(7000)
+                    .sweet(50)
+                    .acidity(10)
+                    .body(50)
+                    .balance(50)
+                    .aftertaste(50)
+                    .aroma(10)
+                    .desc("식물성 크림과 동물성크림을 섞어 만든 달달한 아인슈페너 느낌")
+                    .build();
+
+            Coffee_Board coffee_board2 = Coffee_Board.builder()
+                    .store_name("벙커컴퍼니")
+                    .menu_name("쥬시롱블랙")
+                    .price(5500)
+                    .sweet(35)
+                    .acidity(30)
+                    .body(50)
+                    .balance(50)
+                    .aftertaste(50)
+                    .aroma(45)
+                    .desc("묵직한 바디감이 느껴지고 다크하지만 중간에 적절한 베리한 신맛이 느껴진다.")
+                    .build();
+
+            Coffee_Board coffee_board3 = Coffee_Board.builder()
+                    .store_name("커피몽타주")
+                    .menu_name("아메리카노")
+                    .price(4000)
+                    .sweet(30)
+                    .acidity(20)
+                    .body(40)
+                    .balance(50)
+                    .aftertaste(40)
+                    .aroma(40)
+                    .desc("아직 안마셔봄 ㅋㅋ")
+                    .build();
+
+            Coffee_Board coffee_board4 = Coffee_Board.builder()
+                    .store_name("이디야")
+                    .menu_name("아메리카노")
+                    .price(3300)
+                    .sweet(30)
+                    .acidity(10)
+                    .body(30)
+                    .balance(30)
+                    .aftertaste(20)
+                    .aroma(20)
+                    .desc("에스프레소 농도가 연하다.")
+                    .build();
+
+            em.persist(member1);
+            em.persist(coffee_board1);
+            em.persist(coffee_board2);
+            em.persist(coffee_board3);
+            em.persist(coffee_board4);
         }
     }
 }
